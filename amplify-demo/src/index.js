48a262b4f527a4f0a96125f345c917932b1aae06
dfreire <dario.freire@gmail.com>
2019-02-11 20:49:52 +0000

    first commit

---
+++ amplify-demo/src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Amplify from "aws-amplify";
+
+import config from "./aws-exports";
+import "./index.css";
+import App from "./App";
+
+Amplify.configure(config);
+
+ReactDOM.render(<App />, document.getElementById("root"));
